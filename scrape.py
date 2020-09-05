e221f5d6444217c7747ba2d50cc5fa9f2b461658
IdanHershcovich <idanhershcovich@gmail.com>
2020-09-05 17:56:11 -0500

    Working steam price for regular and sale

--- scrape.py
+++ scrape.py
@@ -3,36 +3,64 @@ import requests as req
 
 
 
+
+def getSteamItemURL(item_name: str):
+    base_url = 'https://store.steampowered.com/search/?term='
+    formatted_name = item_name.replace(' ', '+')
+    item_url = base_url+formatted_name
+    print("return value from getSteamURL() func: " + item_url)
+    return item_url
+
 def getPage(url):
     try:
         response = req.get(url)
         response.raise_for_status()
     except req.exceptions.HTTPError as err:
         raise SystemExit(err)
-
+    print("return value from getPage() func: {} ".format(response))
     return response
 
 def pageToText(page_obj):
     page_text = BeautifulSoup(page_obj.text, 'html.parser')
-
+    
+    # print("return value from pagetoText() func: {}".format(page_text))
     return page_text
 
 def parsePrice(soup):
 
-    game_name = "Buy Monster Hunter: World"
-    test = soup.find_all("h1", string=game_name)
-    print(test)
+    regular_price_class = "col search_price responsive_secondrow"
+    discounted_price_class = "col search_price discounted responsive_secondrow"
+    
 
-    price_in_page = soup.select('.game_purchase_price')
+    price = soup.find_all("div", class_= discounted_price_class)
+    if not price:
+        price = soup.find_all("div", class_= regular_price_class)[0].text
+    else: 
+        price =  soup.find_all("div", class_= discounted_price_class)[0].text
+    # test = soup.find_all("div", class_="col search_price responsive_secondrow")[0].text
+    print(price)
 
-    print(price_in_page)
+    # price_in_page = soup.select('.game_purchase_price')
 
-p = getPage('https://store.steampowered.com/app/582010/Monster_Hunter_World/')
+   
+    # print(type(price_in_page))
 
-t = pageToText(p)
+# p = getPage(getSteamItem('Battlefront'))
 
-parsePrice(t)
+# t = pageToText(p)
+
+# parsePrice(t)
 
 
 
 
+def run():
+    search_item_url = getSteamItemURL('battlefront 2')
+    page_response = getPage(search_item_url)
+    page_text = pageToText(page_response)
+    price = parsePrice(page_text)
+
+
+
+
+run()
